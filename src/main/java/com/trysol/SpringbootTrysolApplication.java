13ca01cf6a020727b698b17e5aec79033f5f1716
Trysol Global Services <127195129+trysol2023@users.noreply.github.com>
2023-03-09 12:20:04 +0530

    newproject

---
+++ src/main/java/com/trysol/SpringbootTrysolApplication.java
@@ -0,0 +1,13 @@
+package com.trysol;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringbootTrysolApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringbootTrysolApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/trysol/SpringbootTrysolApplicationTests.java
@@ -0,0 +1,13 @@
+package com.trysol;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringbootTrysolApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
